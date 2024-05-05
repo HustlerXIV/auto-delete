48d059a23e1a9129a3b5a290ac1a17021c734d04
Napat Jaiyim <napat.jaiyim@coforge.com>
2024-05-05 22:13:59 +0700

    [BUG FIXES] Removed useEffect and used setTimeout in handleAdd instead

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import { data as initialData } from "./utils/data";
 import CustomBtn from "./components/common/CustomBtn/CustomBtn";
@@ -13,37 +13,31 @@ interface Item {
 }
 
 function App() {
-  const [fruitBasket, setFruitBasket] = useState<Item[]>([]);
-  const [vegetBasket, setVegetBasket] = useState<Item[]>([]);
-  const [mainList, setMainList] = useState<Item[]>(initialData);
-  const [removeQueue, setRemoveQueue] = useState<Item[]>([]);
+  const [fruitBasket, setFruitBasket] = React.useState<Item[]>([]);
+  const [vegetBasket, setVegetBasket] = React.useState<Item[]>([]);
+  const [mainList, setMainList] = React.useState<Item[]>(initialData);
+  let timeoutId: NodeJS.Timeout | null = null;
 
-  useEffect(() => {
-    let timer: NodeJS.Timeout | null = null;
-
-    if (removeQueue.length > 0) {
-      timer = setTimeout(() => {
-        const itemToRemove = removeQueue[0];
-        if (itemToRemove.type === FRUIT) {
-          setMainList((prevMainList) => [...prevMainList, fruitBasket[0]]);
-          setFruitBasket((prevFruit) =>
-            prevFruit.filter((f) => f !== itemToRemove)
-          );
-        }
-        if (itemToRemove.type === VEGETABLE) {
-          setMainList((prevMainList) => [...prevMainList, vegetBasket[0]]);
-          setVegetBasket((prevVeget) =>
-            prevVeget.filter((v) => v !== itemToRemove)
-          );
-        }
-        setRemoveQueue((prevQueue) => prevQueue.slice(1));
-      }, 5000);
+  const handleRemove = (item: Item) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    if (item.type === FRUIT) {
+      setFruitBasket((prevFruit) =>
+        prevFruit.filter((f) => f.name !== item.name)
+      );
+    }
+    if (item.type === VEGETABLE) {
+      setVegetBasket((prevVeget) =>
+        prevVeget.filter((v) => v.name !== item.name)
+      );
     }
 
-    return () => {
-      if (timer) clearTimeout(timer);
-    };
-  }, [removeQueue, fruitBasket, vegetBasket]);
+    setMainList((prevMainList) => {
+      const existingItem = prevMainList.find((d) => d === item);
+      return existingItem ? prevMainList : [...prevMainList, item];
+    });
+  };
 
   const handleAdd = (item: Item) => {
     if (item.type === FRUIT) {
@@ -52,42 +46,32 @@ function App() {
     if (item.type === VEGETABLE) {
       setVegetBasket((prevVeget) => [...prevVeget, item]);
     }
+
     setMainList((prevMainList) => prevMainList.filter((d) => d !== item));
-    setRemoveQueue((prevQueue) => [...prevQueue, item]);
+
+    timeoutId = setTimeout(() => {
+      handleRemove(item);
+    }, 5000);
   };
 
-  const handleRemove = (type: string, item: Item) => {
-    setRemoveQueue((prevQueue) => prevQueue.filter((q) => q !== item));
-    if (type === FRUIT) {
-      setFruitBasket((prevFruit) => prevFruit.filter((f) => f !== item));
-    }
-    if (type === VEGETABLE) {
-      setVegetBasket((prevVeget) => prevVeget.filter((v) => v !== item));
-    }
-    setMainList((prevMainList) => {
-      const originalItem = initialData.find((d) => d === item);
-      return originalItem ? [...prevMainList, item] : prevMainList;
-    });
-  };
-
-  const renderItems = (type: string, items: Item[]) => {
+  const renderItems = (items: Item[], catalog: string) => {
     return items.map((item: Item) => (
       <CustomBtn
         key={item?.name}
         text={item?.name}
         onClick={() =>
-          type === MAINLIST ? handleAdd(item) : handleRemove(type, item)
+          catalog === MAINLIST ? handleAdd(item) : handleRemove(item)
         }
       />
     ));
   };
 
-  const renderBasket = (type: string, basket: Item[]) => {
+  const renderBasket = (basket: Item[], catalog: string) => {
     return (
       <div className="responsive-container">
-        <div className="listed-title">{type}</div>
+        <div className="listed-title">{catalog}</div>
         <div className="listed-box">
-          <div className="button-box">{renderItems(type, basket)}</div>
+          <div className="button-box">{renderItems(basket, catalog)}</div>
         </div>
       </div>
     );
@@ -96,10 +80,10 @@ function App() {
   return (
     <div className="main-container">
       <div className="button-box responsive-container">
-        {renderItems(MAINLIST, mainList)}
+        {renderItems(mainList, MAINLIST)}
       </div>
-      {renderBasket(FRUIT, fruitBasket)}
-      {renderBasket(VEGETABLE, vegetBasket)}
+      {renderBasket(fruitBasket, FRUIT)}
+      {renderBasket(vegetBasket, VEGETABLE)}
     </div>
   );
 }
